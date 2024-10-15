b3fd5f5356dc4bdf86a7dad71f7578e8ccbf8825
estebansant <estebansant30@gmail.com>
2024-10-14 21:47:31 -0300

    added navbar links and search

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,11 +1,14 @@
-import { SignedIn, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Theme from "./Theme";
+import MobileNav from "./MobileNav";
+import GlobalSearch from "../search/GlobalSearch";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-6 p-6 shadow-light-300 sm:px-12 dark:shadow-none">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-6 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo.svg"
@@ -14,13 +17,14 @@ const Navbar = () => {
           alt="devflow logo"
         />
 
-        <p className="h2-bold font-spaceGrotesk text-dark-100 max-sm:hidden dark:text-light-900">
+        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
           Dev <span className="text-primary-500">Flow</span>
         </p>
       </Link>
-      {/* GlobalSearch */}
+      <GlobalSearch />
       <div className="flex-between gap-5">
-        Theme
+        <Theme />
+        {/* <SignInButton></SignInButton> */}
         <SignedIn>
           <UserButton
             appearance={{
@@ -33,7 +37,12 @@ const Navbar = () => {
             }}
           />
         </SignedIn>
-        {/* MobileNav */}
+        <SignedOut>
+          <div className="hidden lg:flex">
+            <SignInButton />
+          </div>
+        </SignedOut>
+        <MobileNav />
       </div>
     </nav>
   );
