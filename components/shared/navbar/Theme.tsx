4c990654ec45d2b44e2dc922e42ea1f4d9edab77
estebansant <estebansant30@gmail.com>
2024-10-16 11:41:38 -0300

    started working on the main page

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -18,7 +18,7 @@ const Theme = () => {
   const { mode, setMode } = useTheme();
 
   return (
-    <Menubar className="relative border-none bg-transparent shadow-none">
+    <Menubar className="relative z-50 border-none bg-transparent shadow-none">
       <MenubarMenu>
         <MenubarTrigger className="focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200">
           {mode === "light" ? (
