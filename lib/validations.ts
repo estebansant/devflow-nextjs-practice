bfdd97cb7123c99b0e9860a8025dda5bc5c9ada3
estebansant <estebansant30@gmail.com>
2024-10-17 23:04:44 -0300

    add tag and remove tag functionalities developed

--- lib/validations.ts
+++ lib/validations.ts
@@ -3,5 +3,5 @@ import { z } from "zod";
 export const QuestionsSchema = z.object({
   title: z.string().min(5).max(130),
   explanation: z.string().min(100),
-  tags: z.array(z.string().min(1).max(15)).min(1).max(3), // This is a way to ask for 1 to 3 tags with at most 15 characters each tag
+  tags: z.array(z.string().min(2).max(15)).min(1).max(3), // This is a way to ask for 1 to 3 tags with at most 15 characters each tag
 });
